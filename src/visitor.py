e7e57a629ed1917a257708d3ede04f14e806f4d3
remusao <rerere321@gmail.com>
2012-10-24 13:36:53 +0300

    Update src/visitor.py
    Added a defaultVisitor

--- src/visitor.py
+++ src/visitor.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def visit(tree):
+class DefaultVisitor:
+    """General visitor"""
+    
+    def visit(self, node):
+        """Visit a node"""
+        # Find a specific visit method, default to "default"
+        methname = "visit_%s" % node.__class__.__name__
+        method = getattr(self, methname, self.default)
+
+        # Call visit method on node
+        method(node)
+
+    def default(self, node):
+        """Visit node children"""
+        for child in node.children:
+            self.visit(child)
